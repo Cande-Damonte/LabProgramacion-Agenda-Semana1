073bd1a18bff70f81fc7f6267315453ecb148835
Alumno <Alumno@PC107>
2025-04-23 11:27:56 -0300

    Agregar archivos de proyecto.

---
+++ Agregar_Contacto.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace LabProgramacion_Agenda_Semana1
+{
+    partial class Agregar_Contacto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Contacto = new System.Windows.Forms.GroupBox();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.idContacto = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.lblCorreoElectronico = new System.Windows.Forms.Label();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.cmbCategorias = new System.Windows.Forms.ComboBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.dgvContactos = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.contactoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modificarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Contacto.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContactos)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Contacto
+            // 
+            this.Contacto.Controls.Add(this.treeView);
+            this.Contacto.Controls.Add(this.txtid);
+            this.Contacto.Controls.Add(this.idContacto);
+            this.Contacto.Controls.Add(this.btnAgregar);
+            this.Contacto.Controls.Add(this.txtCorreo);
+            this.Contacto.Controls.Add(this.lblCorreoElectronico);
+            this.Contacto.Controls.Add(this.lblCategoria);
+            this.Contacto.Controls.Add(this.cmbCategorias);
+            this.Contacto.Controls.Add(this.txtTelefono);
+            this.Contacto.Controls.Add(this.txtApellido);
+            this.Contacto.Controls.Add(this.txtNombre);
+            this.Contacto.Controls.Add(this.lblApellido);
+            this.Contacto.Controls.Add(this.lblTelefono);
+            this.Contacto.Controls.Add(this.lblNombre);
+            this.Contacto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Contacto.Location = new System.Drawing.Point(24, 43);
+            this.Contacto.Name = "Contacto";
+            this.Contacto.Size = new System.Drawing.Size(621, 384);
+            this.Contacto.TabIndex = 39;
+            this.Contacto.TabStop = false;
+            this.Contacto.Text = "Contacto";
+            // 
+            // treeView
+            // 
+            this.treeView.Location = new System.Drawing.Point(377, 27);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(236, 329);
+            this.treeView.TabIndex = 33;
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(45, 27);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(127, 29);
+            this.txtid.TabIndex = 31;
+            // 
+            // idContacto
+            // 
+            this.idContacto.AutoSize = true;
+            this.idContacto.Location = new System.Drawing.Point(10, 30);
+            this.idContacto.Name = "idContacto";
+            this.idContacto.Size = new System.Drawing.Size(30, 24);
+            this.idContacto.TabIndex = 30;
+            this.idContacto.Text = "Id:";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(13, 323);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(333, 32);
+            this.btnAgregar.TabIndex = 27;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(184, 237);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(178, 29);
+            this.txtCorreo.TabIndex = 26;
+            // 
+            // lblCorreoElectronico
+            // 
+            this.lblCorreoElectronico.AutoSize = true;
+            this.lblCorreoElectronico.Location = new System.Drawing.Point(10, 240);
+            this.lblCorreoElectronico.Name = "lblCorreoElectronico";
+            this.lblCorreoElectronico.Size = new System.Drawing.Size(173, 24);
+            this.lblCorreoElectronico.TabIndex = 25;
+            this.lblCorreoElectronico.Text = "Correo Electronico:";
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(10, 281);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(95, 24);
+            this.lblCategoria.TabIndex = 24;
+            this.lblCategoria.Text = "Categoria:";
+            // 
+            // cmbCategorias
+            // 
+            this.cmbCategorias.FormattingEnabled = true;
+            this.cmbCategorias.Items.AddRange(new object[] {
+            "Amigos",
+            "Familia",
+            "Trabajo"});
+            this.cmbCategorias.Location = new System.Drawing.Point(121, 280);
+            this.cmbCategorias.Name = "cmbCategorias";
+            this.cmbCategorias.Size = new System.Drawing.Size(197, 32);
+            this.cmbCategorias.TabIndex = 23;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(95, 186);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(126, 29);
+            this.txtTelefono.TabIndex = 22;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(95, 126);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(131, 29);
+            this.txtApellido.TabIndex = 21;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(95, 73);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(131, 29);
+            this.txtNombre.TabIndex = 20;
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(10, 131);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(84, 24);
+            this.lblApellido.TabIndex = 19;
+            this.lblApellido.Text = "Apellido:";
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Location = new System.Drawing.Point(10, 189);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(90, 24);
+            this.lblTelefono.TabIndex = 18;
+            this.lblTelefono.Text = "Telefono:";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(10, 76);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(84, 24);
+            this.lblNombre.TabIndex = 17;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // dgvContactos
+            // 
+            this.dgvContactos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvContactos.Location = new System.Drawing.Point(672, 51);
+            this.dgvContactos.Name = "dgvContactos";
+            this.dgvContactos.RowHeadersWidth = 62;
+            this.dgvContactos.Size = new System.Drawing.Size(268, 373);
+            this.dgvContactos.TabIndex = 38;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.contactoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(952, 24);
+            this.menuStrip1.TabIndex = 40;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // contactoToolStripMenuItem
+            // 
+            this.contactoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.modificarToolStripMenuItem,
+            this.eliminarToolStripMenuItem});
+            this.contactoToolStripMenuItem.Name = "contactoToolStripMenuItem";
+            this.contactoToolStripMenuItem.Size = new System.Drawing.Size(68, 22);
+            this.contactoToolStripMenuItem.Text = "Contacto";
+            // 
+            // modificarToolStripMenuItem
+            // 
+            this.modificarToolStripMenuItem.Name = "modificarToolStripMenuItem";
+            this.modificarToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.modificarToolStripMenuItem.Text = "Modificar";
+            // 
+            // eliminarToolStripMenuItem
+            // 
+            this.eliminarToolStripMenuItem.Name = "eliminarToolStripMenuItem";
+            this.eliminarToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.eliminarToolStripMenuItem.Text = "Eliminar";
+            // 
+            // Agregar_Contacto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.Contacto);
+            this.Controls.Add(this.dgvContactos);
+            this.Name = "Agregar_Contacto";
+            this.Text = "Agregar_Contacto";
+            this.Contacto.ResumeLayout(false);
+            this.Contacto.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContactos)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Contacto;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.Label idContacto;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.Label lblCorreoElectronico;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.ComboBox cmbCategorias;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.DataGridView dgvContactos;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem contactoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modificarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eliminarToolStripMenuItem;
+    }
+}
---
+++ Modificar Contacto.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace LabProgramacion_Agenda_Semana1
+{
+    partial class Modificar_Contacto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblContacto = new System.Windows.Forms.GroupBox();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.idContacto = new System.Windows.Forms.Label();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.lblCorreoelectronico = new System.Windows.Forms.Label();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.cmbCategorias = new System.Windows.Forms.ComboBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.dgvContactos = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.contactoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modificarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblContacto.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContactos)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblContacto
+            // 
+            this.lblContacto.Controls.Add(this.treeView);
+            this.lblContacto.Controls.Add(this.txtid);
+            this.lblContacto.Controls.Add(this.idContacto);
+            this.lblContacto.Controls.Add(this.btnModificar);
+            this.lblContacto.Controls.Add(this.txtCorreo);
+            this.lblContacto.Controls.Add(this.lblCorreoelectronico);
+            this.lblContacto.Controls.Add(this.lblCategoria);
+            this.lblContacto.Controls.Add(this.cmbCategorias);
+            this.lblContacto.Controls.Add(this.txtTelefono);
+            this.lblContacto.Controls.Add(this.txtApellido);
+            this.lblContacto.Controls.Add(this.txtNombre);
+            this.lblContacto.Controls.Add(this.lblApellido);
+            this.lblContacto.Controls.Add(this.lblTelefono);
+            this.lblContacto.Controls.Add(this.lblNombre);
+            this.lblContacto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContacto.Location = new System.Drawing.Point(10, 26);
+            this.lblContacto.Name = "lblContacto";
+            this.lblContacto.Size = new System.Drawing.Size(685, 425);
+            this.lblContacto.TabIndex = 42;
+            this.lblContacto.TabStop = false;
+            this.lblContacto.Text = "Contacto:";
+            // 
+            // treeView
+            // 
+            this.treeView.Location = new System.Drawing.Point(381, 27);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(294, 367);
+            this.treeView.TabIndex = 33;
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(45, 27);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(162, 29);
+            this.txtid.TabIndex = 31;
+            // 
+            // idContacto
+            // 
+            this.idContacto.AutoSize = true;
+            this.idContacto.Location = new System.Drawing.Point(10, 30);
+            this.idContacto.Name = "idContacto";
+            this.idContacto.Size = new System.Drawing.Size(30, 24);
+            this.idContacto.TabIndex = 30;
+            this.idContacto.Text = "Id:";
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(21, 355);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(267, 38);
+            this.btnModificar.TabIndex = 27;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(184, 237);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(178, 29);
+            this.txtCorreo.TabIndex = 26;
+            // 
+            // lblCorreoelectronico
+            // 
+            this.lblCorreoelectronico.AutoSize = true;
+            this.lblCorreoelectronico.Location = new System.Drawing.Point(10, 240);
+            this.lblCorreoelectronico.Name = "lblCorreoelectronico";
+            this.lblCorreoelectronico.Size = new System.Drawing.Size(173, 24);
+            this.lblCorreoelectronico.TabIndex = 25;
+            this.lblCorreoelectronico.Text = "Correo Electronico:";
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(10, 291);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(95, 24);
+            this.lblCategoria.TabIndex = 24;
+            this.lblCategoria.Text = "Categoria:";
+            // 
+            // cmbCategorias
+            // 
+            this.cmbCategorias.FormattingEnabled = true;
+            this.cmbCategorias.Items.AddRange(new object[] {
+            "Amigos",
+            "Familia",
+            "Trabajo"});
+            this.cmbCategorias.Location = new System.Drawing.Point(117, 291);
+            this.cmbCategorias.Name = "cmbCategorias";
+            this.cmbCategorias.Size = new System.Drawing.Size(225, 32);
+            this.cmbCategorias.TabIndex = 23;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(99, 187);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(126, 29);
+            this.txtTelefono.TabIndex = 22;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(95, 126);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(131, 29);
+            this.txtApellido.TabIndex = 21;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(95, 73);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(131, 29);
+            this.txtNombre.TabIndex = 20;
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(10, 131);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(84, 24);
+            this.lblApellido.TabIndex = 19;
+            this.lblApellido.Text = "Apellido:";
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Location = new System.Drawing.Point(10, 189);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(90, 24);
+            this.lblTelefono.TabIndex = 18;
+            this.lblTelefono.Text = "Telefono:";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(10, 76);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(84, 24);
+            this.lblNombre.TabIndex = 17;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // dgvContactos
+            // 
+            this.dgvContactos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvContactos.Location = new System.Drawing.Point(712, 48);
+            this.dgvContactos.Name = "dgvContactos";
+            this.dgvContactos.RowHeadersWidth = 62;
+            this.dgvContactos.Size = new System.Drawing.Size(301, 371);
+            this.dgvContactos.TabIndex = 41;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.contactoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(1101, 24);
+            this.menuStrip1.TabIndex = 43;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // contactoToolStripMenuItem
+            // 
+            this.contactoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.modificarToolStripMenuItem,
+            this.eliminarToolStripMenuItem});
+            this.contactoToolStripMenuItem.Name = "contactoToolStripMenuItem";
+            this.contactoToolStripMenuItem.Size = new System.Drawing.Size(68, 22);
+            this.contactoToolStripMenuItem.Text = "Contacto";
+            // 
+            // modificarToolStripMenuItem
+            // 
+            this.modificarToolStripMenuItem.Name = "modificarToolStripMenuItem";
+            this.modificarToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.modificarToolStripMenuItem.Text = "Agregar";
+            // 
+            // eliminarToolStripMenuItem
+            // 
+            this.eliminarToolStripMenuItem.Name = "eliminarToolStripMenuItem";
+            this.eliminarToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.eliminarToolStripMenuItem.Text = "Eliminar";
+            // 
+            // Modificar_Contacto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 459);
+            this.Controls.Add(this.lblContacto);
+            this.Controls.Add(this.dgvContactos);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Modificar_Contacto";
+            this.Text = "Modificar_Contacto";
+            this.lblContacto.ResumeLayout(false);
+            this.lblContacto.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContactos)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox lblContacto;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.Label idContacto;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.Label lblCorreoelectronico;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.ComboBox cmbCategorias;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.DataGridView dgvContactos;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem contactoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modificarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eliminarToolStripMenuItem;
+    }
+}
